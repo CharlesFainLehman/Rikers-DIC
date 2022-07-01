a65aa21cd0361586be22da4be5824161e758f03b
Charles Fain Lehman <clehman@manhattan-institute.org>
2022-07-01 13:12:29 -0400

    better debugging in the fetch script

--- bin/fetch_dic.py
+++ bin/fetch_dic.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from datetime import date
+from datetime import datetime
 
 #note that the limit is 50,000 for the request is 50K. If for some reason
 #there are more than 50K people on Rikers, this will break. But, uh, that's unlikely.
 todays_prisoners = pd.read_json('https://data.cityofnewyork.us/resource/7479-ugqb.json?$limit=50000', storage_options = {'X-App-Token':'p7zbW0RmUIiLtNo3XeA39pGY8'})
+
+print("got file at" + datetime.now())
 
 #DOC_Inmates_InCustody_Daily_20160602
 todays_prisoners.to_csv("./dat/via_github/DOC_Inmates_InCustody_Daily_" + date.today().strftime("%Y%m%d"))
