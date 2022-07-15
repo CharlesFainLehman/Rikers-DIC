522eb2736b68b067da7f1af2d876999074433d1b
CharlesFainLehman <charles.fain.lehman@gmail.com>
2022-07-15 13:38:03 -0400

    changing the manually downloaded files, because the old ones were messed up. A few missing, but they're complete.

--- dat/manual/rename_manual_files.py
+++ dat/manual/rename_manual_files.py
@@ -4,7 +4,7 @@ from datetime import date
 from datetime import datetime
 
 for f in os.listdir():
-    if re.match("dic-", f):
-        file_date_str = re.findall("(\w+) (\d\d), (\d\d\d\d)", f)
-        file_date_date = date(int(file_date_str[0][2]), datetime.strptime(file_date_str[0][0], '%B').month, int(file_date_str[0][1]))
+    if re.match("Rikers", f):
+        file_date_str = re.findall("(\d\d)-(\d\d)-(\d\d)", f)
+        file_date_date = date(int(file_date_str[0][2]) + 2000, int(file_date_str[0][0]), int(file_date_str[0][1]))
         os.rename(f,"DOC_Inmates_InCustody_Daily_" + file_date_date.strftime("%Y%m%d") + ".csv")
